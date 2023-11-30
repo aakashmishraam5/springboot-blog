6b35f3f19b7f3dc45bda730e7d5fa46033eaa2e7
akashmishra <aakashmishraam5@gmail.com>
2023-11-30 21:39:16 +0530

    blog

---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,81 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+import jakarta.persistence.PostRemove;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    public PostServiceImpl (PostRepository postRepository){
+        this.postRepository=postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        //convert DTO to entity
+        Post post =mapToEntity(postDto);
+
+
+        Post newPost=postRepository.save(post);
+
+        //convert entitiy to DTO
+        PostDto postResponse=mapToDTO(newPost);
+
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts=postRepository.findAll();
+        return  posts.stream().map(post -> mapToDTO(post)).collect(Collectors.toList());
+
+
+    }
+    //convert entity to DTO
+    private PostDto mapToDTO(Post post){
+        PostDto postDto =new PostDto();
+        postDto.setContent(postDto.getContent());
+        postDto.setId(post.getId());
+        postDto.setDescription(postDto.getDescription());
+        postDto.setTitle(post.getTitle());
+        return postDto;
+    }
+    //convert DTO to entity
+    private Post mapToEntity(PostDto postDto){
+        Post post=new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        return post;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
